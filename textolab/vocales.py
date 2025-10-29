18f3b2952990a2a93f72dc1c44e45d8218ff8de9
Lautaro Belo <75724981+ExeBelo@users.noreply.github.com>
2025-10-29 20:23:41 -0300

    Revert "Modulo vocales - Lautaro belo"

--- textolab/vocales.py
+++
@@ -1,8 +0,0 @@
-def contar_vocales(texto:str):
-    texto = texto.lower()
-    vocales = ["a", "e", "i", "o", "u"]
-    contador_vocales = 0
-    for letra in texto:
-        if letra in vocales:
-            contador_vocales += 1
-    return contador_vocales
